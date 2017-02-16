5adf710937bf5788ccc36066427eca9916907b72
cloudfiers <jericmagaway@yahoo.com.ph>
2017-02-16 11:33:12 +0800

    first commit

---
+++ MVCFilter/Context/ItemContext.cs
@@ -0,0 +1,15 @@
+﻿using MVCFilter.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVCFilter.Context
+{
+    public class ItemContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+        
+    }
+}
---
+++ MVCFilter/Models/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCFilter.Models
+{
+    public class Item
+    {
+        public int ID { get; set; }
+        public string  Name { get; set; }
+        public string Description { get; set; }
+        [Required]
+        [DataType(DataType.DateTime, ErrorMessage="Required Date")]
+        public DateTime Date { get; set; }
+
+    }
+}
